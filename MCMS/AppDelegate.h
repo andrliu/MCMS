b3923cc231fbe473b44a3f63dcbda140d1165d58
Andrew Liu <dieseliu321@gmail.com>
2014-10-28 15:04:44 -0700

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Andrew Liu on 10/28/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
